5ff65384c4743149ad726975b1932c6aed258d00
Amirhossein <amir.kingstars@gmail.com>
2024-10-08 15:36:46 +0330

    commit

---
+++ src/PlanetType.java
@@ -0,0 +1,15 @@
+public enum PlanetType {
+    ROCK("سنگی"),
+    GAS("گازی");
+
+    private final String persianName;
+
+    PlanetType(String persianName) {
+        this.persianName = persianName;
+    }
+
+    @Override
+    public String toString() {
+        return persianName;
+    }
+}
